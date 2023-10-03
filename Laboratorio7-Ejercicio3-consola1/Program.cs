befe30c0e29f5620189151652be9917bfe771678
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-10-03 18:16:38 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7-Ejercicio3-consola1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7_Ejercicio3_consola1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Los años bisiestos desde el año 1900 son :");
+
+            for (int i = 1900; i <= 2023; i++)
+            {
+                Console.Write(i +", ") ;
+                i = i + 3 ;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
